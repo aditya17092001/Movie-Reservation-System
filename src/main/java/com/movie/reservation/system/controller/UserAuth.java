8463089dd462f162d65bd937145a9ff3bc01a4d0
Aditya Udata <adityaudata@gmail.com>
2026-02-02 20:36:15 +0530

    feat: Added Reset-Password and loggers,  appended request_id in logger

--- src/main/java/com/movie/reservation/system/controller/UserAuth.java
+++ src/main/java/com/movie/reservation/system/controller/UserAuth.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.movie.reservation.system.constants.RestURLs;
+import com.movie.reservation.system.dto.ResetPasswordRequestDTO;
 import com.movie.reservation.system.model.Users;
 import com.movie.reservation.system.repository.UserRepo;
 
@@ -30,16 +31,16 @@ public class UserAuth {
 
     @PostMapping(RestURLs.SIGNUP)
     public ResponseEntity<?> userSignup(@RequestBody Users user) {
+        HashMap<String, String> response = new HashMap<>();
         if(user.getEmail() == null || user.getEmail().isEmpty() || user.getPassword() == null || user.getPassword().isEmpty()) {
+            response.put("message", "Email and Password can't be empty.");
             log.info("Email and Password can't be empty");
             return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
-                .body("Email and Password can't be empty.");
+                .body(response);
         }
 
         Users user_existing = repository.findByEmail(user.getEmail());
-
-        HashMap<String, String> response = new HashMap<>();
 
         if(user_existing != null) {
             log.info("User already exist");
@@ -52,36 +53,78 @@ public class UserAuth {
 
         String password = user.getPassword();
         String encryptedPassword = passwordEncoder.encode(password);
-        log.info("Encrypted Password: "+encryptedPassword);
+
         user.setPassword(encryptedPassword);
         repository.save(user);
 
-        log.info("User saved successfully");
-
+        log.info("User registered successfully");
+        response.put("message", "User registered successfully.");
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(user);
+                .body(response);
     }
 
 
     @PostMapping(RestURLs.SIGNIN)
     public ResponseEntity<?> userSignin(@RequestBody Users user) {
-        if(user.getEmail() == null || user.getEmail().isEmpty() || user.getPassword() == null || user.getPassword().isEmpty()) return ResponseEntity
+        HashMap<String, String> response = new HashMap<>();
+        if(user.getEmail() == null || user.getEmail().isEmpty() || user.getPassword() == null || user.getPassword().isEmpty()) {
+            log.info("Email and Password can't be empty");
+            response.put("message", "Email and Password can't be empty.");
+            return ResponseEntity
             .status(HttpStatus.UNAUTHORIZED)
-            .body("Email and Password can't be empty.");
+            .body(response);
+        }
 
         Users existing_user = repository.findByEmail(user.getEmail());
         
         if(existing_user != null && passwordEncoder.matches(user.getPassword(), existing_user.getPassword())) {
+            log.info("Signin successful.");
+            response.put("message", "Signin successful.");
             return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(existing_user);
+                .body(response);
         } else {
+            log.info("Invalid Credentials.");
+            response.put("message", "Invalid Credentials.");
             return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body("Invalid Credentials.");
+                .body(response);
         }
     }
-    
-    
+
+    @PostMapping(RestURLs.RESET_PASSWORD)
+    public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordRequestDTO resetPasswordRequestDTO) {
+        Users user = repository.findByEmail(resetPasswordRequestDTO.getEmail());
+
+        HashMap<String, String> response = new HashMap<>();
+        if(user == null) {
+            log.info("User not found with this email.");
+            response.put("message", "User not found with this email.");
+            return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+        }
+
+        if(!passwordEncoder.matches(resetPasswordRequestDTO.getCurrentPassword(), user.getPassword())) {
+            log.info("Current password is incorrect.");
+            response.put("message", "Current password is incorrect.");
+
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+        }
+
+        String updatedPassword = resetPasswordRequestDTO.getUpdatePassword();
+        String encryptedPassword = passwordEncoder.encode(updatedPassword);
+        user.setPassword(encryptedPassword);
+
+        repository.save(user);
+
+        log.info("Password reset successfully.");
+        response.put("message", "Password reset successfully.");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
 }
